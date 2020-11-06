e9cd1ce6155bdf6fe6b09206f1763d43c6a8d419
Tangeni Shikomba <tangenishikomba@gmail.com>
2020-11-06 12:40:09 -0500

    initial project commit

---
+++ config.py
@@ -0,0 +1,1 @@
+api_token = '3c36747e8eff4738a93420c1bbccb9f4'
---
+++ main.py
@@ -0,0 +1,10 @@
+# import modules
+# %% [Modules]
+
+# define functions here
+# %% [Functions]
+
+# implement main method
+# %% [Main Method]
+if __name__ == "__main__":
+    pass
