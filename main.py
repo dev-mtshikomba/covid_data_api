89bc958f4480eb6d8ccfd4648d85795ae07e4f9e
Tangeni Shikomba <tangenishikomba@gmail.com>
2020-11-07 13:46:28 -0500

    project update

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 # %% [Modules]
 from config import *
 import requests
-
+import csv
+import pandas as pd
 # define functions here
 # %% [Functions]
 
@@ -28,7 +29,40 @@ def pull_state_data_from_api(state=None):
         return requests.get('https://api.covidactnow.org/v2/state/'+state+'.json?apiKey='+api_token).json()
     return requests.get('https://api.covidactnow.org/v2/states.json?apiKey='+api_token).json()
 
+
+def write_data_to_csv_file(json_object):
+    # now we will open a file for writing
+    data_file = open('counties_data_1.csv', 'w')
+
+    # create the csv writer object
+    csv_writer = csv.writer(data_file)
+
+    # Counter variable used for writing
+    # headers to the CSV file
+    count = 0
+
+    for e in json_object:
+        if count == 0:
+            # Writing headers of CSV file
+            header = str(e.keys())
+            print(header)
+            csv_writer.writerow(e.keys())
+            count += 1
+
+        # Writing data of CSV file
+        csv_writer.writerow(e.values())
+
+    data_file.close()
+
+# AWS Lambda Function
+# %% [Lambda Function]
+
+
+def main(event=None, contex=None):
+    write_data_to_csv_file(pull_counties_data_from_api())
+
+
 # implement main method
 # %% [Main Method]
 if __name__ == "__main__":
-    pull_counties_data_from_api()
+    main()
