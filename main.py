53e36b5ad380d13b8ae4d152398a6155b18f0215
Tangeni Shikomba <tangenishikomba@gmail.com>
2020-11-25 11:56:05 -0500

    updated main file

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from config import *
 import requests
 import csv
 import boto3
+import s3fs
+
 # define functions here
 # %% [Functions]
 
@@ -15,8 +17,8 @@ def pull_counties_data_from_api(county=None):
                - List of county FIPS codes (https://www.nrcs.usda.gov/wps/portal/nrcs/detail/national/home/?cid=nrcs143_013697)
     '''
     if county:
-        return requests.get('https://api.covidactnow.org/v2/county/'+str(county)+'.json?apiKey='+api_token).json()
-    return requests.get('https://api.covidactnow.org/v2/counties.json?apiKey='+api_token).json()
+        return requests.get('https://api.covidactnow.org/v2/county/' + str(county) + '.json?apiKey=' + api_token).json()
+    return requests.get('https://api.covidactnow.org/v2/counties.json?apiKey=' + api_token).json()
 
 
 def pull_state_data_from_api(state=None):
@@ -26,43 +28,59 @@ def pull_state_data_from_api(state=None):
                - List of state postal codes (https://www.nrcs.usda.gov/wps/portal/nrcs/detail/?cid=nrcs143_013696)
     '''
     if state:
-        return requests.get('https://api.covidactnow.org/v2/state/'+state+'.json?apiKey='+api_token).json()
-    return requests.get('https://api.covidactnow.org/v2/states.json?apiKey='+api_token).json()
+        return requests.get('https://api.covidactnow.org/v2/state/' + state + '.json?apiKey=' + api_token).json()
+    return requests.get('https://api.covidactnow.org/v2/states.json?apiKey=' + api_token).json()
 
 
 def write_data_to_csv_file(json_object):
     # now we will open a file for writing
-    data_file = open('counties_data.csv', 'w')
 
-    # create the csv writer object
-    csv_writer = csv.writer(data_file)
-
-    # Counter variable used for writing
-    # headers to the CSV file
-    count = 0
-
-    for e in json_object:
-        if count == 0:
-            # Writing headers of CSV file
-            header = str(e.keys())
-            print(header)
-            csv_writer.writerow(e.keys())
-            count += 1
-
-        # Writing data of CSV file
-        csv_writer.writerow(e.values())
-
-    data_file.close()
-
-# AWS Lambda Function
-# %% [Lambda Function]
-
-def main(event=None, contex=None):
-    write_data_to_csv_file(pull_counties_data_from_api())
+    # properly call your s3 bucket
     s3 = boto3.resource('s3',
                         aws_access_key_id='AKIA4DMTNGFHKBJEISGU',
                         aws_secret_access_key='cN7IRF9OEL2rzyFQ+UJte3/+tuLErhBKgSXJU1mn')
-    s3.Bucket('covid-data').upload_file('counties_data.csv', 'counties_data.csv')
+    bucket = s3.Bucket('covid-data')
+    key = 'counties.csv'
+
+    obj = list(bucket.objects.filter(Prefix=key))
+    if len(obj) > 0:
+        print("Exists")
+        s3.Object('covid-data', 'counties_data.csv').delete()
+    else:
+        print("Not Exists")
+    fs = s3fs.S3FileSystem(anon=True)
+    # data_file = open('counties_data.csv', 'w')
+    with fs.open('s3://covid-data/counties.csv', 'w', newline='') as f:
+        # create the csv writer object
+        csv_writer = csv.writer(f)
+
+        # Counter variable used for writing
+        # headers to the CSV file
+        count = 0
+
+        for e in json_object:
+            if count == 0:
+                # Writing headers of CSV file
+                header = str(e.keys())
+                print(header)
+                csv_writer.writerow(e.keys())
+                count += 1
+
+            # Writing data of CSV file
+            csv_writer.writerow(e.values())
+    #bucket.upload_file('counties_data.csv', key)
+    # data_file.close()
+
+
+# AWS Lambda Function
+# %% [Lambda Function]
+def main(event=None, contex=None):
+    write_data_to_csv_file(pull_counties_data_from_api())
+    # s3 = boto3.resource('s3',
+    #                    aws_access_key_id='AKIA4DMTNGFHKBJEISGU',
+    #                    aws_secret_access_key='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')
+    # s3.Bucket('covid-data').upload_file('counties_data.csv', 'counties_data.csv')
+
 
 # implement main method
 # %% [Main Method]
